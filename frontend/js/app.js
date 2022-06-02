145d94a08183e58c16def1c166d9f41294cfc0ed
batangeth <batang.eth.mini@gmail.com>
2022-06-02 21:21:42 +0800

    update

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -304,7 +304,7 @@ function setTotalPrice() {
   // const totalPriceWei = BigInt(info.deploymentConfig.mintPrice) * BigInt(mintInputValue);
   
   // const totalPriceWei = BigInt(maxPerMint) * BigInt(mintInputValue);
-  const totalPriceWei = BigInt(maxPerMint) * BigInt(mintInputValue);
+  const totalPriceWei = BigNumber(maxPerMint) * BigInt(mintInputValue);
   
   let priceType = '';
   if(chain === 'rinkeby') {

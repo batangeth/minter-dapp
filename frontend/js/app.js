dd874e81f3664b40378383bd732fb8fcfa0f5792
batangeth <batang.eth.mini@gmail.com>
2022-06-02 22:44:10 +0800

    update

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -335,53 +335,7 @@ async function mint() {
   const publicMintActive = await contract.methods.mintingOpen().call();
   const presaleMintActive = await contract.methods.onlyAllowlistMode().call();
 
-  if (publicMintActive) {
-    // PUBLIC MINT
-    try {
-      // const mintTransaction = await contract.methods
-      //   .mint(amount)
-      //   .send({ from: window.address, value: value.toString() });
-
-      const mintTransaction = await contract.methods
-        .mintToMultiple(window.address, amount)
-        .send({ from: window.address, value: value.toString() });
-
-      if(mintTransaction) {
-        if(chain === 'polygon') {
-          const url = `https://polygonscan.com/tx/${mintTransaction.transactionHash}`;
-          const mintedContainer = document.querySelector('.minted-container');
-          const countdownContainer = document.querySelector('.countdown');
-          const mintedTxnBtn = document.getElementById("mintedTxnBtn");
-          mintedTxnBtn.href = url;
-          countdownContainer.classList.add('hidden');
-          mintedContainer.classList.remove('hidden');
-        } else if (chain === 'rinkeby') {
-          const url = `https://rinkeby.etherscan.io/tx/${presaleMintTransaction.transactionHash}`;
-          const mintedContainer = document.querySelector('.minted-container');
-          const countdownContainer = document.querySelector('.countdown');
-          const mintedTxnBtn = document.getElementById("mintedTxnBtn");
-          mintedTxnBtn.href = url;
-          countdownContainer.classList.add('hidden');
-          mintedContainer.classList.remove('hidden');
-        }
-        console.log("Minuted successfully!", `Transaction Hash: ${mintTransaction.transactionHash}`);
-      } else {
-        const mainText = document.getElementById("mainText");
-        mainText.innerText = mint_failed;
-        mintButton.innerText = button_public_mint;
-        mintButton.disabled = false;
-
-        console.log("Failed to mint!");
-      }
-    } catch(e) {
-      const mainText = document.getElementById("mainText");
-      mainText.innerText = mint_failed;
-      mintButton.innerText = button_public_mint;
-      mintButton.disabled = false;
-
-      console.log(e);
-    }
-  } else if (presaleMintActive) {
+   if (presaleMintActive) {
     // PRE-SALE MINTING
     try {
       const merkleData = await fetch(
@@ -429,6 +383,52 @@ async function mint() {
       mintButton.disabled = false;
 
       // console.log(e);
+    }
+  } else if (publicMintActive) {
+    // PUBLIC MINT
+    try {
+      // const mintTransaction = await contract.methods
+      //   .mint(amount)
+      //   .send({ from: window.address, value: value.toString() });
+
+      const mintTransaction = await contract.methods
+        .mintToMultiple(window.address, amount)
+        .send({ from: window.address, value: value.toString() });
+
+      if(mintTransaction) {
+        if(chain === 'polygon') {
+          const url = `https://polygonscan.com/tx/${mintTransaction.transactionHash}`;
+          const mintedContainer = document.querySelector('.minted-container');
+          const countdownContainer = document.querySelector('.countdown');
+          const mintedTxnBtn = document.getElementById("mintedTxnBtn");
+          mintedTxnBtn.href = url;
+          countdownContainer.classList.add('hidden');
+          mintedContainer.classList.remove('hidden');
+        } else if (chain === 'rinkeby') {
+          const url = `https://rinkeby.etherscan.io/tx/${presaleMintTransaction.transactionHash}`;
+          const mintedContainer = document.querySelector('.minted-container');
+          const countdownContainer = document.querySelector('.countdown');
+          const mintedTxnBtn = document.getElementById("mintedTxnBtn");
+          mintedTxnBtn.href = url;
+          countdownContainer.classList.add('hidden');
+          mintedContainer.classList.remove('hidden');
+        }
+        console.log("Minuted successfully!", `Transaction Hash: ${mintTransaction.transactionHash}`);
+      } else {
+        const mainText = document.getElementById("mainText");
+        mainText.innerText = mint_failed;
+        mintButton.innerText = button_public_mint;
+        mintButton.disabled = false;
+
+        console.log("Failed to mint!");
+      }
+    } catch(e) {
+      const mainText = document.getElementById("mainText");
+      mainText.innerText = mint_failed;
+      mintButton.innerText = button_public_mint;
+      mintButton.disabled = false;
+
+      console.log(e);
     }
   }
 }

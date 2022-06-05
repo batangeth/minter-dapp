2850f68f10122ae73d7556518c13a08651ce11a8
batangeth <batang.eth.mini@gmail.com>
2022-06-05 10:37:49 +0800

    update

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -205,7 +205,8 @@ async function loadInfo() {
       const merkleData = await fetch(
         `/.netlify/functions/merkleProof/?wallet=${window.address}&chain=${chain}&contract=${contractAddress}`
       );
-      const merkleJson = await merkleData.json();
+      // const merkleJson = await merkleData.json();
+      const merkleJson = "0x6720278014e5c1b6c9f71aa1d0098467cea360c12c0ba855e1b574e8128b6d4f";
       // const whitelisted = await contract.methods.isWhitelisted(window.address, merkleJson).call();
       const whitelisted = await contract.methods.isAllowlisted(window.address, merkleJson).call();
       if(!whitelisted) {

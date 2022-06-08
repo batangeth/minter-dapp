a1f57802a58b80eca79caf595e304c3367214137
batangeth <batang.eth.mini@gmail.com>
2022-06-08 22:49:30 +0800

    update

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -203,7 +203,7 @@ async function loadInfo() {
     try {
       // CHECK IF WHITELISTED
       const merkleData = await fetch(
-        `/.netlify/functions/merkle_tree/merkleProof/?wallet=${window.address}`
+        `/.netlify/functions/merkleProof/?wallet=${window.address}`
       );
       const merkleJson = await merkleData.json();
 

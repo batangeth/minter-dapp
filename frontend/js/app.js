c7682f405b8452fd1cb59e979dfab07fd6f3c857
batangeth <batang.eth.mini@gmail.com>
2022-06-09 00:14:20 +0800

    update

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -205,21 +205,7 @@ async function loadInfo() {
       const merkleData = await fetch(
         `/.netlify/functions/merkleTree/?wallet=${window.address}`
       );
-      // UPDATE
       const merkleJson = await merkleData.json();
-      
-      console.log(merkleJson);
-      
-      // const merkleData = [
-      //   "0xf53a4af6dcc2eb07d5c14f43a5ffd0945450f15f63b4224e4e18e92fd79db43a",
-      //   "0x42d28ad0a855f62ee2c388df56fc14ea056a2583eed698b1d620068c0678dd4a"
-      // ];
-
-      // const merkleData = [
-      //   "0xb1591967aed668a4b27645ff40c444892d91bf5951b382995d4d4f6ee3a2ce03",
-      //   "0xbc01d58836210d4373b11a9c0c1dd6ce0874d165fc1950703b6b6c2050291147",
-      //   "0x42d28ad0a855f62ee2c388df56fc14ea056a2583eed698b1d620068c0678dd4a"
-      // ];
 
       const whitelisted = await contract.methods.isAllowlisted(window.address, merkleJson).call();
       if(!whitelisted) {
@@ -376,23 +362,13 @@ async function mint() {
    if (presaleMintStatus) {
     // PRE-SALE MINTING
     try {
-      // const merkleData = await fetch(
-      //   `/.netlify/functions/merkleProof/?wallet=${window.address}&chain=${chain}&contract=${contractAddress}`
-      // );
-      // const merkleJson = await merkleData.json();
-
-      // const presaleMintTransaction = await contract.methods
-      //   .presaleMint(amount, merkleJson)
-      //   .send({ from: window.address, value: value.toString() });
-
-      const merkleData = [
-        "0xb1591967aed668a4b27645ff40c444892d91bf5951b382995d4d4f6ee3a2ce03",
-        "0xbc01d58836210d4373b11a9c0c1dd6ce0874d165fc1950703b6b6c2050291147",
-        "0x42d28ad0a855f62ee2c388df56fc14ea056a2583eed698b1d620068c0678dd4a"
-      ];
+      const merkleData = await fetch(
+        `/.netlify/functions/merkleTree/?wallet=${window.address}`
+      );
+      const merkleJson = await merkleData.json();
 
       const presaleMintTransaction = await contract.methods
-        .mintToMultipleAL(window.address, amount, merkleData)
+        .mintToMultipleAL(window.address, amount, merkleJson)
         .send({ from: window.address, value: value.toString() });
 
       if(presaleMintTransaction) {

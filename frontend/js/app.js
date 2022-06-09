f1b060e854bd254084cae158af22391d99c3a57e
batangeth <batang.eth.mini@gmail.com>
2022-06-09 21:07:57 +0800

    update

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,7 +1,3 @@
-const createAlchemyWeb3 = require('@alch/alchemy-web3');
-
-const Web3Alc = createAlchemyWeb3("https://eth-rinkeby.alchemyapi.io/v2/mphIhoXE1E_OWkC5P7ZyiHNAwVncW2HF");
-
 let accounts;
 
 // METAMASK CONNECTION

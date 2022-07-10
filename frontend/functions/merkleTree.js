64d930ddaec47d7dcd4d0d491dae5453d22529da
batangeth <batang.eth.mini@gmail.com>
2022-07-10 15:02:23 +0800

    update

--- frontend/functions/merkleTree.js
+++ frontend/functions/merkleTree.js
@@ -213,7 +213,7 @@ exports.handler = async (event, context) => {
   ];
 
   const wallet = event.queryStringParameters && event.queryStringParameters.wallet
-  const leafNodes = WLAddresses.map(addr => keccak256(addr));
+  const leafNodes = FMAddresses.map(addr => keccak256(addr));
   const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
   const claimingAddress = keccak256(wallet);
   const hexProof = merkleTree.getHexProof(claimingAddress);
